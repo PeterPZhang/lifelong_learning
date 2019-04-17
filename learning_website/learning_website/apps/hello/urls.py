7c4bbd8f39450ffc93bfdee88282ad8f8ae34d22
PeterMBP <peterpz0428@163.com>
2019-04-17 17:08:56 +0800

    路由应用及试图方法

---
+++ learning_website/learning_website/apps/hello/urls.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+"""
+__author__ = 'peter'
+__mtime__ = '2019-04-17'
+# Follow the master,become a master.
+             ┏┓       ┏┓
+            ┏┛┻━━━━━━━┛┻┓
+            ┃    ☃      ┃
+            ┃  ┳┛   ┗┳  ┃
+            ┃     ┻     ┃
+            ┗━┓       ┏━┛
+              ┃       ┗━━━━┓
+              ┃ 神兽保佑     ┣┓
+              ┃　永无BUG！   ┏┛
+              ┗┓┓┏━━━┳┓┏━━━┛
+               ┃┫┫   ┃┫┫
+               ┗┻┛   ┗┻┛
+"""
+from django.urls import path
+from .views import *
+
+app_name = 'hello'
+
+urlpatterns = [
+    path('', Hello.as_view(), name='hello'),
+]
