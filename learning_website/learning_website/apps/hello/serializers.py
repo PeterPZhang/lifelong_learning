9ad21ed011c71f731172b53b059af3797947acd0
PeterMBP <peterpz0428@163.com>
2019-04-17 17:08:01 +0800

    加入序列化器

---
+++ learning_website/learning_website/apps/hello/serializers.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+"""
+__author__ = 'peter'
+__mtime__ = '2019-04-17'
+# Follow the master,become a master.
+             ┏┓       ┏┓
+            ┏┛┻━━━━━━━┛┻┓
+            ┃    ☃      ┃
+            ┃  ┳┛   ┗┳  ┃
+            ┃     ┻     ┃
+            ┗━┓       ┏━┛
+              ┃       ┗━━━━┓
+              ┃ 神兽保佑     ┣┓
+              ┃　永无BUG！   ┏┛
+              ┗┓┓┏━━━┳┓┏━━━┛
+               ┃┫┫   ┃┫┫
+               ┗┻┛   ┗┻┛
+"""
+from rest_framework import serializers
+
+
+# class HelloSerializer(serializers):
+#     """初始序列化器"""
+#     class Meta:
+
+
